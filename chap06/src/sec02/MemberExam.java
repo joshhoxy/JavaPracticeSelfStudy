459d0b66a73b75fa3c50c3405f726d4c41aef82d
joshhoxy <63964410+joshhoxy@users.noreply.github.com>
2022-03-11 14:50:03 +0900

    0311 java_study_class

---
+++ chap06/src/sec02/MemberExam.java
@@ -0,0 +1,15 @@
+package sec02;
+
+public class MemberExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member member = new Member();
+		member.name = "최하얀";
+		member.age = 23;
+		
+		System.out.println("member1의 이름: " + member.name );
+		System.out.println("member1의 나이: " + member.age);
+	}
+
+}
